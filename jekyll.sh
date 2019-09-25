de6238efeb190d41bb8a250eb997b121810bc660
Amit Gupta <amit.gupta@arrowts.com>
2019-09-25 16:45:38 -0700

    updated website

--- jekyll.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-bundle exec jekyll serve --drafts
-#bundle exec jekyll serve
---
+++ js/script2.js
@@ -0,0 +1,11 @@
+(function(global) {
+    var typed = new Typed('.profile-summary h1', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 100,
+        backSpeed: 40,
+        backDelay: 2000,
+        smartBackspace: true,
+        loop: true,
+        showCursor: false,
+    });
+})(window);
